52c6006392add28ac3f920bbf307cc89f7dc418d
Roby Karti S <55522784+robykartis@users.noreply.github.com>
2023-10-08 19:16:02 +0700

    FRist

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
+
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind(), partytown()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
+});
